ea154b4383391e716643373fb889901fd51ca8f3
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-15 13:47:47 +0600

    Create BucketList file

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Sergey Shcheglov on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
