f76273e7e06a86631bbe0f1e2f7c2f72cfcf6ee3
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-15 14:39:39 +0600

    add onTapGesture to write smth to storage"

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,11 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
     }
+}
+
+struct ContentView: View {
+    let users = [
+        User(firstName: "Sergey", lastName: "Shcheglov"),
+        User(firstName: "Pavel", lastName: "Zagorodnikh"),
+        User(firstName: "Grigory", lastName: "Moroz"),
+    ].sorted()
+    
+    var body: some View {
+        Text("Hello, World")
+            .onTapGesture {
+                let str = "Test message"
+                let url = FileManager().getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+//        func getDocumentsDirectory() -> URL {
+//            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//            return paths[0]
+//        }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
